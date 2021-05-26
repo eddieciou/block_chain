496853d8dc5063c160619b698f4b8632a8b4c43c
eddieciou <a589658gur@gmail.com>
2021-05-26 15:11:12 +0800

    調整架構

---
+++ commons/Transaction.py
@@ -0,0 +1,9 @@
+
+
+class Transaction:
+    def __init__(self, sender, receiver, amounts, fee, message):
+        self.sender = sender
+        self.receiver = receiver
+        self.amounts = amounts
+        self.fee = fee
+        self.message = message
